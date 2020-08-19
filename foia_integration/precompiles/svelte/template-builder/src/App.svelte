f7dc804d72d1c5852eafb2bc08291059e7cafe1c
Max Lee <maxbmhlee@gmail.com>
2020-08-19 09:49:48 -0400

    stylized template

--- foia_integration/precompiles/svelte/template-builder/src/App.svelte
+++ foia_integration/precompiles/svelte/template-builder/src/App.svelte
@@ -1,63 +1,119 @@
 <script>
-import { each, onMount } from "svelte/internal";
+import { each, onMount, text } from "svelte/internal";
 
 	export let buttonItems;
-	let outputJson = {
-		extras: []
-	};
-	// this refers to all of the boilerplate (ie everything that isn't in one of the buttons)
-	let parsedText = "";
+	let nodes = [];
 
-	function handleTextChange(event) {
+	function handleTextInputChange(event) {
+		adjustHeight(event);
+		updateInput();
 	}
 
-	function updateJsonText() {
-		if (outputJson.extras.length === 0) {
-			parsedText = document.getElementById("target").value;
-		} else {
-			const lastElem = outputJson.extras[outputJson.length - 1];
-			// the parsed text is all of the text minus the text that's in the buttons
-			// so if we add the button element lengths to the last position, we get the index
-			// within the textarea text where our new text starts
-			const sumElemLengths = outputJson.extras.map(d => d.text.length)
-				.reduce((a, b) => a + b, 0);
-			const newTextStartIdx = sumElemLengths + parsedText.length;
-			parsedText += document.getElementById("target").value.slice(newTextStartIdx);
+	function adjustHeight(event) {
+		const elem = event.target;
+		// change the height of element if it overflows
+		if (elem.clientHeight < elem.scrollHeight) {
+			elem.style.height = elem.scrollHeight + 20 + "px";
+		} 
+	}
+
+	function updateInput() {
+		const regex = /\{\{(.*)\}\}/g;
+		const elemText = document.getElementById("target").value;
+		let newNodes = [];
+		let lastIdx = 0;
+		let field;
+		while (field = regex.exec(elemText)) {
+			const beforeText = field.input.slice(lastIdx, field.index);
+			newNodes.push({type: "text", content: beforeText});
+			newNodes.push({type: "plugin", content: field[1]});
+			lastIdx = field.index + field[0].length;
 		}
+		newNodes.push({type: "text", content: elemText.slice(lastIdx)});
+		nodes = newNodes;
 	}
 
 	function addItem(event) {
-		updateJsonText();
-		const elemText = event.target.textContent;
-		document.getElementById("target").value += elemText;
-		outputJson.extras.push({
-			field: event.target.getAttribute("data-field"),
-			position: parsedText.length,
-			text: event.target.textContent
-		});
-		outputJson.text = parsedText;
+		const elemText = event.target.innerText;
+		document.getElementById("target").value += `{{${elemText}}}`;
+		updateInput();
 	}
 </script>
 
+<div class="presentation__area">
+	{#each nodes as textNode}
+		{#if textNode.type === "text"}
+		{textNode.content}
+		{:else}
+		<span class="template__highlight">{textNode.content}</span>
+		{/if}
+	{/each}
+</div>
 <div class="button__container">
 {#each Object.keys(buttonItems) as button}
 <button data-field={button} class="button__item" on:click={addItem}>{buttonItems[button]}</button>
 {/each}
 </div>
 <div class="form__area">
-	<textarea id="target" rows="25" cols="100" on:input={handleTextChange}></textarea>
+	<textarea id="target" on:input={handleTextInputChange}></textarea>
 </div>
 <div class="submit__container"></div>
 
 <style>
-	textarea {
-		outline: none;
+	* {
+		margin: auto;
+	}
+
+	#target {
+		box-sizing: border-box;
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		text-align: left;
+		font-size: 1.2rem;
+		width: 80%;
 		resize: none;
-		overflow: auto;
-		font-size: 1.5rem;
+		margin-top: 1rem;
+		min-height: 200px;
+		overflow: hidden;
+		display: block;
+		margin-left: auto;
+	}
+
+	.template__highlight {
+		background-color: #ffca7a;
+		border-radius: 5px;
+		padding: 3px;
+	}
+
+	.presentation__area {
+		min-height: 100px;
+		white-space: pre-line;
+		font-size: 1.3rem;
+		border-bottom: 2px solid rgb(187,187,187);
+		padding: 5px;
+		width: 80%;
 	}
 
 	.button__container {
-		min-height: 200px;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		margin: 5px
+	}
+
+	.button__item {
+		font-size: 1.2rem;
+		padding: 3px;
+		border: none;
+		margin: 2px;
+		background-color: #ffca7a;
+		transition: background-color 2s, color 2s;
+		border-radius: 5px;
+	}
+
+	.button__item:hover, .button__item:focus {
+		background-color: #C27400;
+		cursor: pointer;
+		color: #FAFAFA;
 	}
 </style>
